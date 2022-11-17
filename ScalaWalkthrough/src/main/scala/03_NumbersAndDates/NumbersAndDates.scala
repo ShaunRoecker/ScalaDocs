2d5c729a418d9bd29f771ec4ec7091bde24a5dc3
s-roecker <94014218+s-roecker@users.noreply.github.com>
2022-11-16 19:07:32 -0500

    11/16/22 work added (finished Numbers And Dates section)

--- ScalaWalkthrough/src/main/scala/03_NumbersAndDates/NumbersAndDates.scala
+++ ScalaWalkthrough/src/main/scala/03_NumbersAndDates/NumbersAndDates.scala
@@ -16,12 +16,15 @@ object NumbersAndDates extends App {
         // comparingFloatingPointNumbers()
         // handlingLargeNumbers()
         // generatingRandomNumbers()
-        formattingNumbersAndCurrency()
-
+        // formattingNumbersAndCurrency()
+        // creatingNewDateAndTimeInstances()
+        // CalculatingTheDifferenceBetweenTwoDates()
+        // formattingDates()
+        // parsingStringsIntoDates()
 
     }
     scalaNumDate()
-    //
+    // Underscores In Numeric Literals
     def underScoresInNumericLiterals(): Unit = {
         // Can use underscore to represent numeric literals
 
@@ -64,7 +67,7 @@ object NumbersAndDates extends App {
 
         // for complex numbers, check out "https://typelevel.org/spire"      
     }
-    //
+    //Parsing A Number From A String
     def parsingANumberFromAString(): Unit = {
         //Problem: You want to convert a string to one of Scala's numeric types
         "1".toByte
@@ -106,7 +109,7 @@ object NumbersAndDates extends App {
         def makeIntFP_try(s: String): Try[Int] = Try(s.toInt)
 
     }
-    //
+    //Converting Between Numeric Types: Casting
     def convertingBetweenNumericTypes_Casting(): Unit = {
         // Problem: You want to convert from one numeric type to another
         val b: Byte = 1
@@ -119,14 +122,14 @@ object NumbersAndDates extends App {
 
         // x.asInstanceOf[Float]
     }
-    //
+    //Overriding The Default Numeric Type
     def overridingTheDefaultNumericType(): Unit = {
         // Problem: When using an implicit type declaration style, Scala automatically assigns
         // types based on their numeric values, and you need to override the default type declaration   
         // when you create a numeric field.
 
     }
-    //
+    // Comparing Floating Point Numbers
     def comparingFloatingPointNumbers(): Unit = {
         val a = 0.1 + 0.2
         println(a)  // 0.30000000000000004
@@ -193,7 +196,7 @@ object NumbersAndDates extends App {
         // What every programmer should know about floating-point arithmatic -"Dogs, to the layperson"
         
     }
-    //
+    // Handling Large Numbers
     def handlingLargeNumbers(): Unit = {
         // Problem: You're writting an application and need to use very large integer or decimal numbers
         println("HANDLING LARGE NUMBERS")
@@ -405,6 +408,267 @@ object NumbersAndDates extends App {
         println(formatterDefault.format(12.34)) //12.34
         println(formatterDefault.format(1_200.34)) //1,200.34
         println(formatterDefault.format(1_245_656.34)) //1,245,656.34
+
+    }
+    // Creating Date and Time Instances from Java Classes and 
+    // using the Instance class to build a timer
+    def creatingNewDateAndTimeInstances(): Unit = {
+        // Problem: You need to create new date and time instances
+        // using the Date and Time API that was introduces with Java 8
+        // Ref: https://oreil.ly/7T8dh
+        import java.time.*
+        println("CREATING NEW DATE AND TIME INSTANCES")
+        println(LocalDate.now) //2022-11-16
+        println(LocalTime.now) //12:57:22.790298
+        println(LocalDateTime.now) //2022-11-16T12:57:22.790445
+        println(Instant.now) //2022-11-16T17:57:22.790547Z
+        println(ZonedDateTime.now) //2022-11-16T12:57:22.792938-05:00[America/New_York]
+
+        // Past or Future  //ref: https://oreil.ly/qXo7f
+        val squirrelDay = LocalDate.of(2020, 1, 21) //2020-01-21
+        println(squirrelDay)
+        val squirrelDay2 = LocalDate.of(2020, Month.JANUARY, 21) //2020-01-21
+        println(squirrelDay2)
+        val squirrelDay3 = LocalDate.of(2020, 1, 21).plusDays(20) //2020-02-10
+        println(squirrelDay3)
+
+        //java.time.LocalTime(https://oreil.lyTNNWb) has 5 of* factory methods, including these
+        // LocalTime.of(hour: Int, minute: Int)
+        // LocalTime.of(hour: Int, minute: Int, second: Int)
+
+        println(LocalTime.of(0, 0)) //00:00
+        println(LocalTime.of(0, 1)) // 00:01
+        println(LocalTime.of(1, 1)) //01:01
+        println(LocalTime.of(23, 59)) //23:59
+
+        //java.time.LocalDateTime(https://oreil.ly/eHIH4) has 9 of* factory method constructors,
+        // including these
+        //LocalDateTime.of(year: Int, month: Int, dayOfMonth: Int, hour: Int, minute: Int)
+        //LocalDateTime.of(year: Int, month: Month, dayOfMonth: Int, hour: Int, minute: Int)
+        //LocalDateTime.of(date: LocalDate, time: LocalTime)
+
+
+        //java.time.ZonedDateTime(https://oreil.ly/nmoY3) has 5 of* factory methods, including these
+        //ZonedDateTime.of(int year, int month, int dayOfMonth, int hour, int minute, int second, 
+            //int nanosecond, ZoneId zone)
+        //ZonedDateTime.of(LocalDate date, LocalTime time, ZoneId zone)  ** used below **
+        //ZonedDateTime.of(Instant instant, ZoneId zone)
+
+        // Example of the second method:
+        val zdt = ZonedDateTime.of(
+            LocalDate.now,
+            LocalTime.now,
+            ZoneId.of("America/New_York")
+        )
+        println(zdt)  //2022-11-16T13:58:06.191462-05:00[America/New_York]
+        // ref(zone IDs) https://oreil.ly/h7A9T
+
+        // The Instant Class
+        // The instant class is nice for many reasons, including giving you the ability to
+        // calculate the time duration between two instants 
+        import java.time.{Instant, Duration}
+
+        val start = Instant.now
+        Thread.sleep(2_000)
+        val stop = Instant.now
+        val delta = Duration.between(start, stop)
+        println(delta)           // PT2.003014S
+        println(delta.toMillis)  // 2003
+        println(delta.toNanos)   // 2003014000
         
     }
+    // Calculating the difference between two dates
+    def CalculatingTheDifferenceBetweenTwoDates(): Unit = {
+        // Problem: You need to calculate the difference bewteen two dates
+        println("Calculating the difference between two dates")
+        import java.time.LocalDate
+        import java.time.temporal.ChronoUnit.*
+
+        val now = LocalDate.of(2022, 11, 16)
+        val xmas = LocalDate.of(2022, 12, 25)
+
+        println(DAYS.between(now, xmas)) // 39
+        println(MONTHS.between(now, xmas)) // 1
+        println(YEARS.between(now, xmas)) // 0
+
+        val nextXmas = LocalDate.of(2023, 12, 25)
+        val years: Long = YEARS.between(now, nextXmas)
+        println(years) // 1
+        println(DAYS.between(now, nextXmas)) // 404
+
+        // Using the same LocalDate values, you can also use the Period class,
+        // but notice the significant difference in the output between the ChronoUnit
+        // and Period Approaches
+
+        import java.time.Period
+
+        val diff = Period.between(now, nextXmas)
+        println(diff.getYears) // 1
+        println(diff.getMonths) // 1
+        println(diff.getDays) // 9
+        // This Period method measures the difference of the specific time unit,
+        // such as date1 month vs. date2 month, not the difference as a whole
+
+        // The between method of ChronoUnit:
+        import java.time.LocalDateTime
+        import java.time.temporal.ChronoUnit
+
+        // of(year, month, dayOfMonth, hour, minute)
+        val d1 = LocalDateTime.of(2020, 1, 1, 1, 1)
+        val d2 = LocalDateTime.of(2063, 4, 5, 1, 1)
+
+        println(ChronoUnit.DAYS.between(d1, d2)) // 15800
+        println(ChronoUnit.MONTHS.between(d1, d2)) // 519
+        println(ChronoUnit.YEARS.between(d1, d2)) // 43
+        println(ChronoUnit.MINUTES.between(d1, d2)) // 22752000
+        println(ChronoUnit.SECONDS.between(d1, d2)) // 1365120000
+
+        // The ChronoUnit class has many other enum constants, including CENTURIES, DECADES,
+        // HOURS, MICROS, MILLIS, WEEKS, and more.
+
+
+    }
+    // Formatting dates
+    def formattingDates(): Unit = {
+        // Problem: You need to print dates in a desired format
+        println("FORMATTING DATES")
+        import java.time.LocalDate
+        import java.time.format.DateTimeFormatter
+
+        val d = LocalDate.now 
+        println(d) // 2022-11-16
+        val f = DateTimeFormatter.BASIC_ISO_DATE
+        println(f.format(d)) // 20221116
+
+        // Other Formatters:
+        //    ISO_LOCAL_DATE  //2021-02-04
+        //    ISO_DATE        //2021-02-04
+        //    BASIC_ISO_DATE  //20210204
+        //    ISO_ORDINAL_DATE  //2021-035
+        //    ISO_WEEK_DATE  //2021-W05-4
+
+        //  Locale Formatters
+        //  ofLocalizedDate
+        //  ofLocalizedTime
+        //  ofLocalizedDateTime
+
+        // java.time.format.FormatStyle (https://oreil.ly/K50kH)
+        // (SHORT, MEDIUM, LONG, FULL)
+
+        // import java.time.LocalDate
+        import java.time.format.{DateTimeFormatter, FormatStyle}
+
+        val d2 = LocalDate.of(2021, 1, 1)
+        val fFull = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)
+        println(fFull.format(d2)) // Friday, January 1, 2021
+
+        val fShort = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)
+        println(fShort.format(d2)) // 1/1/21
+
+        // Custom Patterns with ofPattern
+        val d3 = LocalDate.now
+        val f3 = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        println(f3.format(d3)) // 2022-11-16
+
+        val f4 = DateTimeFormatter.ofPattern("MM/dd/yyyy")
+        println(f4.format(d3)) // 11/16/2022
+
+        val f5 = DateTimeFormatter.ofPattern("MMM dd, yyyy")
+        println(f5.format(d3)) // Nov 16, 2022
+
+        val f6 = DateTimeFormatter.ofPattern("E, MMM dd yyyy")
+        println(f6.format(d3)) // Wed, Nov 16 2022
+
+        //  This example demonstrates how to format LocalTime:
+        import java.time.LocalTime
+        val t = LocalTime.now
+        val ft = DateTimeFormatter.ofPattern("h:mm a")
+        println(ft.format(t)) //3:03 PM
+        // Other time formats: 
+            // "HH:mm:ss a"
+
+        // Formatting LocalDateTime
+        import java.time.LocalDateTime
+        val dt = LocalDateTime.now
+        val fdt = DateTimeFormatter.ofPattern("MMM dd, yyyy h:mm a")
+        
+        println(fdt.format(dt)) // Nov 16, 2022 3:05 PM
+
+    }
+    // Parsing Strings into Dates
+    def parsingStringsIntoDates(): Unit = {
+        println("Parsing strings into dates")
+        // Problem: You need to parse a string into one of the date/time types 
+        // introduced in Java 8
+        // ** If your string is in the expected format, pass it to the parse method
+        // of the desired class.  If the string is not in the expected (default) format,
+        // create a formatter to define the format you want to accept.
+
+        // LocalDate
+        import java.time.LocalDate
+        val d = LocalDate.parse("2020-12-10") // LocalDate = 2020-12-10
+        println(d) //2020-12-10
+        // if val d = LocalDate.parse("2020/12/10") // Error: DateTimeParseException
+
+        import java.time.format.DateTimeFormatter
+        val df = DateTimeFormatter.ofPattern("yyy/MM/dd")
+        val d2 = LocalDate.parse("2020/12/10", df) //2020-12-10
+        println(d2)
+
+        // LocalTime
+        import java.time.LocalTime
+        val t = LocalTime.parse("01:02") //01:02
+        val t2 = LocalTime.parse("13:02:03") //13:02:03
+        println(t) //
+        println(t2)
+
+        // Notice each field requires a leading 0
+        // println(LocalTime.parse("1:01"))// Error
+
+        println(LocalTime.parse("00:00", DateTimeFormatter.ISO_TIME)) //00:00
+        println(LocalTime.parse("23:59", DateTimeFormatter.ISO_LOCAL_TIME)) //23:59
+        println(LocalTime.parse("23 59 59", DateTimeFormatter.ofPattern("HH mm ss"))) //23:59:59
+        println(LocalTime.parse("11 59 59 PM", DateTimeFormatter.ofPattern("hh mm ss a"))) //23:59:59
+
+        // LocalDateTime
+        import java.time.LocalDateTime
+        // Default LocalDateTime format
+        val s = "2021-01-01T12:13:14"
+        val ldt = LocalDateTime.parse(s) //2021-01-01T12:13:14
+        println(ldt)
+
+        val s2 = "1999-12-31 23:59"
+        val fdtf = DateTimeFormatter.ofPattern("yyy-MM-dd HH:mm")
+        val ldt2 = LocalDateTime.parse(s2, fdtf) //1999-12-31T23:59
+        println(ldt2)
+
+        val y = "1999-12-31 11:59:59 PM"
+        val z = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss a")
+        val x = LocalDateTime.parse(y, z) // 1999-12-31T23:59:59
+        println(x)
+
+        // Instant
+        // java.time.Instant only has one parse method that requires a date/time stamp
+        // in the proper format
+        import java.time.Instant
+        println(Instant.parse("1970-01-01T00:01:02.00Z")) //1970-01-01T00:01:02Z
+        println(Instant.parse("2021-01-22T23:59:59.00Z")) //2021-01-22T23:59:59Z
+
+        // ZonedDateTime
+        // Demonstrate the default parse methods for java.time.ZonedDateTime
+        import java.time.ZonedDateTime
+        println(ZonedDateTime.parse("2020-12-13T23:59:59-06:00")) //2020-12-13T23:59:59-06:00
+        println(ZonedDateTime.parse("2020-12-13T23:59:59-00:00[US/Mountain]")) //2020-12-13T16:59:59-07:00[US/Mountain]
+
+        import java.time.format.DateTimeFormatter.*
+
+        val zdt = ZonedDateTime.parse("2021-01-01T01:02:03Z", ISO_ZONED_DATE_TIME) //2021-01-01T01:02:03Z
+        println(zdt)
+        println(ZonedDateTime.parse("2021-01-01T23:59:59+01:00", ISO_DATE_TIME)) //2021-01-01T23:59:59+01:00
+        println(ZonedDateTime.parse("2021-03-29T00:59:59-01:00", ISO_OFFSET_DATE_TIME)) //2021-03-29T00:59:59-01:00
+        println(ZonedDateTime.parse("Sat, 29 Feb 2020 00:01:02 GMT", RFC_1123_DATE_TIME)) //2020-02-29T00:01:02Z
+
+
+    }
+
 }
