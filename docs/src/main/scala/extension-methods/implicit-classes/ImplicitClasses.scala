b028e8bd073e98042be4e9c57c2451f60e47ba43
s-roecker <94014218+s-roecker@users.noreply.github.com>
2023-03-21 01:02:42 -0400

    create TypeClasses.scala

---
+++ docs/src/main/scala/extension-methods/implicit-classes/ImplicitClasses.scala
@@ -0,0 +1,57 @@
+package extensionmethods.implicitclasses
+
+
+object ImplicitClasses {
+    // Implicit classes give us the ability to extend the functionality of existing types
+    object Implicits {
+        implicit class IntExtender(val value: Int) extends AnyVal { self =>
+            // we can then create methods in this class that the compiler will turn to
+            // if it can't find the method on the Int companion object.
+            def calculateTo100 = 
+                100 - value
+
+        }
+
+        implicit class StringExtender(val value: String) extends AnyVal { self =>
+            def returnDigits: String = 
+                value.filter(_.isDigit).mkString
+
+            def sumDigitsInString = 
+                value.filter(_.isDigit).map(s => s.toString.toInt).sum
+
+            def sumDigitsInString2 = 
+                value.filter(_.isDigit).map(s => (s.toInt - 48)).sum
+
+        }
+    }
+    import Implicits._
+
+
+    val distFrom100 = 
+        52.calculateTo100
+
+    println(distFrom100) // 48
+
+    println(-30.calculateTo100) // 130
+
+    val extractedDigits = 
+        "Hello56 World8f".returnDigits
+
+    println(extractedDigits) //568
+
+    val sumDigits = 
+        "Hello56 World8f".sumDigitsInString
+
+    println(sumDigits) // 19
+
+    val sumDigits2 = 
+        "Hello56 World8f".sumDigitsInString2
+
+    println(sumDigits2) // 19
+
+    val sumDigitsDist100 = 
+        "Hello56 World8f".sumDigitsInString.calculateTo100
+    println(sumDigitsDist100) // 81   
+
+
+}
