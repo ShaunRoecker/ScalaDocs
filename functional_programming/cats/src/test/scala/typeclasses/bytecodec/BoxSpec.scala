03181a1802019356cf905832190577e28557c560
s-roecker <shaun.roecker@gmail.com>
2023-07-11 17:40:30 -0400

    cats box test cont.

---
+++ functional_programming/cats/src/test/scala/typeclasses/bytecodec/BoxSpec.scala
@@ -0,0 +1,40 @@
+
+import cats._
+import cats.implicits._
+import org.scalacheck.{Gen, Arbitrary}
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.prop.Configuration
+import org.typelevel.discipline.scalatest.FunSuiteDiscipline
+import testing.box.Box
+import testing.box.Box._
+import org.scalacheck.Arbitrary
+import cats.kernel.laws.discipline.EqTests
+import cats.laws.discipline.MonadTests
+
+
+class BoxSpec extends AnyFunSuite with Configuration with FunSuiteDiscipline {
+    val genInt: Gen[Int] = Gen.choose(min=1, max=10)
+    val genInt2: Gen[Int] = Gen.oneOf(t0=1, t1=5, tn=10)
+    val genString: Gen[String] = Gen.alphaNumStr
+    val genString2: Gen[String] = Gen.numStr
+    val genTuple: Gen[(Int, String)] = 
+        for {
+            i <- genInt
+            s <- genString
+        } yield (i, s)
+
+    val arbInt: Arbitrary[Int] = Arbitrary(genInt)
+
+    implicit def arbBoxA[A](implicit arbA: Arbitrary[A]): Arbitrary[Box[A]] =
+        Arbitrary(arbA.arbitrary.map(Box.apply))
+
+    implicit def arbFun[A](implicit arbA: Arbitrary[A]): Arbitrary[A => A] =
+        Arbitrary(arbA.arbitrary.map(a => (_: A) => a))
+
+    checkAll("Eq[Box[Int]]", EqTests[Box[Int]].eqv)
+    checkAll("Monad[Box]", MonadTests[Box].monad[Int, Int, Int])
+    
+
+}
+
+
